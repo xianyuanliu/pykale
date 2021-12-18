4f1f46b730034615aeb2d7a97afb7c687f41d961
Xianyuan <19544506+XianyuanLiu@users.noreply.github.com>
2021-12-18 16:20:37 +0800

    add c3d

---
+++ kale/embed/video_c3d.py
@@ -0,0 +1,184 @@
+# =============================================================================
+# Author: Xianyuan Liu, xianyuan.liu@outlook.com
+# =============================================================================
+
+"""
+Define Convolutional 3D ConvNets(C3D) on Action Recognition.
+Created by Xianyuan Liu from modifying https://github.com/piergiaj/pytorch-i3d/blob/master/pytorch_i3d.py and
+https://github.com/deepmind/kinetics-i3d/blob/master/i3d.py
+"""
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+
+from kale.embed.video_selayer import get_selayer
+
+model_urls = {
+    "c3d_pretrained": "../../../examples/data/c3d-pretrained.pth",
+}
+
+
+class C3D(nn.Module):
+    """
+    The C3D network as described in
+    Tran, Du, et al. "Learning spatiotemporal features with 3d convolutional networks."
+    Proceedings of the IEEE international conference on computer vision. 2015.
+    """
+
+    def __init__(self, num_classes=8, input_channel=3, pretrained=False):
+        super(C3D, self).__init__()
+
+        self.conv1 = nn.Conv3d(input_channel, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+
+        self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv5a = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv5b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
+
+        self.fc6 = nn.Linear(8192, 4096)
+        self.fc7 = nn.Linear(4096, 4096)
+        # self.fc8 = nn.Linear(4096, num_classes)
+
+        self.dropout = nn.Dropout(p=0.5)
+
+        self.relu = nn.ReLU()
+
+        self.__init_weight()
+
+        if pretrained:
+            self.__load_pretrained_weights()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))  # batch_size, 3, 16, 112, 112
+        x = self.pool1(x)
+
+        x = self.relu(self.conv2(x))  # batch_size, 64, 16, 56, 56
+        x = self.pool2(x)
+
+        x = self.relu(self.conv3a(x))  # batch_size, 128, 8, 28, 28
+        x = self.relu(self.conv3b(x))
+        x = self.pool3(x)
+
+        x = self.relu(self.conv4a(x))  # batch_size, 256, 4, 14, 14
+        x = self.relu(self.conv4b(x))
+        x = self.pool4(x)
+
+        x = self.relu(self.conv5a(x))  # batch_size, 512, 2, 7, 7
+        x = self.relu(self.conv5b(x))
+        x = self.pool5(x)
+
+        x = x.view(-1, 8192)  # batch_size, 512, 1, 4, 4
+        x = self.relu(self.fc6(x))  # batch_size, 8192
+        x = self.dropout(x)
+        x = self.relu(self.fc7(x))  # batch_size, 4096
+        x = self.dropout(x)
+
+        # logits = self.fc8(x)
+
+        return x
+
+    def __init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm3d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def __load_pretrained_weights(self):
+        """Initialiaze network."""
+        corresp_name = {
+            # Conv1
+            "features.0.weight": "conv1.weight",
+            "features.0.bias": "conv1.bias",
+            # Conv2
+            "features.3.weight": "conv2.weight",
+            "features.3.bias": "conv2.bias",
+            # Conv3a
+            "features.6.weight": "conv3a.weight",
+            "features.6.bias": "conv3a.bias",
+            # Conv3b
+            "features.8.weight": "conv3b.weight",
+            "features.8.bias": "conv3b.bias",
+            # Conv4a
+            "features.11.weight": "conv4a.weight",
+            "features.11.bias": "conv4a.bias",
+            # Conv4b
+            "features.13.weight": "conv4b.weight",
+            "features.13.bias": "conv4b.bias",
+            # Conv5a
+            "features.16.weight": "conv5a.weight",
+            "features.16.bias": "conv5a.bias",
+            # Conv5b
+            "features.18.weight": "conv5b.weight",
+            "features.18.bias": "conv5b.bias",
+            # fc6
+            "classifier.0.weight": "fc6.weight",
+            "classifier.0.bias": "fc6.bias",
+            # fc7
+            "classifier.3.weight": "fc7.weight",
+            "classifier.3.bias": "fc7.bias",
+        }
+
+        p_dict = torch.load(Path(__file__).absolute().joinpath(model_urls["c3d_pretrained"]))
+        s_dict = self.state_dict()
+        for name in p_dict:
+            if name not in corresp_name:
+                continue
+            s_dict[corresp_name[name]] = p_dict[name]
+        self.load_state_dict(s_dict)
+
+
+def get_1x_lr_params(model):
+    """
+    This generator returns all the parameters for conv and two fc layers of the net.
+    """
+    b = [model.conv1, model.conv2, model.conv3a, model.conv3b, model.conv4a, model.conv4b,
+         model.conv5a, model.conv5b, model.fc6, model.fc7]
+    for i in range(len(b)):
+        for k in b[i].parameters():
+            if k.requires_grad:
+                yield k
+
+
+def get_10x_lr_params(model):
+    """
+    This generator returns all the parameters for the last fc layer of the net.
+    """
+    b = [model.fc8]
+    for j in range(len(b)):
+        for k in b[j].parameters():
+            if k.requires_grad:
+                yield k
+
+
+def c3d(pretrained=False, **kwargs):
+    return C3D(pretrained=pretrained)
+
+
+def se_c3d(attention, pretrained=False, **kwargs):
+    model = C3D(pretrained=pretrained)
+
+    se_layer = get_selayer(attention)
+    model.conv5b.add_module(attention, se_layer(512))
+    return model
+
+
+if __name__ == "__main__":
+    inputs = torch.rand(1, 3, 16, 112, 112)
+    net = C3D(num_classes=101, pretrained=True)
+
+    outputs = net.forward(inputs)
+    print(outputs.size())
