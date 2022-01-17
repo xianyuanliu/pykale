a59eec9da6c1f545f4883d974c94b358c91af941
Xianyuan Liu <19544506+XianyuanLiu@users.noreply.github.com>
2022-01-17 21:27:59 +0800

    add FCANet & ECANet

--- kale/embed/video_selayer.py
+++ kale/embed/video_selayer.py
@@ -9,6 +9,7 @@ channel-wise (SELayerMC), multi-pooling-based channel-wise (SELayerMAC)
 
 [Redundancy and repeat of code will be reduced in the future.]
 """
+import math
 
 import torch
 import torch.nn as nn
@@ -137,9 +138,9 @@ class SRM(nn.Module):
         self.bn = nn.BatchNorm2d(channel)
         self.activation = nn.Sigmoid()
 
-        setattr(self.cfc, 'srm_param', True)
-        setattr(self.bn.weight, 'srm_param', True)
-        setattr(self.bn.bias, 'srm_param', True)
+        setattr(self.cfc, "srm_param", True)
+        setattr(self.bn.weight, "srm_param", True)
+        setattr(self.bn.bias, "srm_param", True)
 
     def forward(self, x, eps=1e-5):
         b, c, _, _ = x.size()
@@ -162,6 +163,8 @@ class SRM(nn.Module):
 
 
 class SRMVideo(SRM):
+    """This module extends SRM for videos."""
+
     def __init__(self, channel):
         super(SRMVideo, self).__init__(channel)
         self.bn = nn.BatchNorm3d(self.channel)
@@ -187,7 +190,7 @@ class SRMVideo(SRM):
 
 
 class CBAM(nn.Module):
-    """Construct Convolutional block attention module.
+    """Construct Convolutional Block Attention Module.
 
     References:
         [1] Woo, Sanghyun, Jongchan Park, Joon-Young Lee, and In So Kweon. "Cbam: Convolutional block attention
@@ -248,8 +251,7 @@ class CBAMSpatialModule(nn.Module):
 
 
 class CBAMVideo(nn.Module):
-    """This module extends CBAM for videos by applying 3D layers.
-    """
+    """This module extends CBAM for videos by applying 3D layers."""
 
     def __init__(self, channel, reduction=16):
         super(CBAMVideo, self).__init__()
@@ -296,6 +298,191 @@ class CBAMSpatialModuleVideo(CBAMSpatialModule):
 class CBAMChannelPool(nn.Module):
     def forward(self, x):
         return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
+
+
+class FCANet(nn.Module):
+    """Construct Frequency Channel Attention Module.
+
+    References:
+        Qin, Zequn, Pengyi Zhang, Fei Wu, and Xi Li. "Fcanet: Frequency channel attention networks."
+        In Proceedings of the IEEE/CVF International Conference on Computer Vision, pp. 783-792. 2021.
+
+        https://github.com/cfzd/FcaNet/blob/master/model/layer.py
+    """
+
+    def __init__(self, channel, dct_h, dct_w, reduction=16, freq_sel_method="top16"):
+        super(FCANet, self).__init__()
+        self.reduction = reduction
+        self.dct_h = dct_h
+        self.dct_w = dct_w
+
+        mapper_x, mapper_y = self.get_freq_indices(freq_sel_method)
+        self.num_split = len(mapper_x)
+        mapper_x = [temp_x * (dct_h // 7) for temp_x in mapper_x]
+        mapper_y = [temp_y * (dct_w // 7) for temp_y in mapper_y]
+        # make the frequencies in different sizes are identical to a 7x7 frequency space
+        # eg, (2,2) in 14x14 is identical to (1,1) in 7x7
+
+        self.dct_layer = MultiSpectralDCTLayer(dct_h, dct_w, mapper_x, mapper_y, channel)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        n, c, h, w = x.shape
+        x_pooled = x
+        if h != self.dct_h or w != self.dct_w:
+            x_pooled = torch.nn.functional.adaptive_avg_pool2d(x, (self.dct_h, self.dct_w))
+            # If you have concerns about one-line-change, don't worry.   :)
+            # In the ImageNet models, this line will never be triggered.
+            # This is for compatibility in instance segmentation and object detection.
+        y = self.dct_layer(x_pooled)
+
+        y = self.fc(y).view(n, c, 1, 1)
+        return x * y.expand_as(x)
+
+    def get_freq_indices(self, method):
+        assert method in [
+            "top1",
+            "top2",
+            "top4",
+            "top8",
+            "top16",
+            "top32",
+            "bot1",
+            "bot2",
+            "bot4",
+            "bot8",
+            "bot16",
+            "bot32",
+            "low1",
+            "low2",
+            "low4",
+            "low8",
+            "low16",
+            "low32",
+        ]
+        num_freq = int(method[3:])
+        if "top" in method:
+            top_x = [0, 0, 6, 0, 0, 1, 1, 4, 5, 1, 3, 0, 0, 0, 3, 2, 4, 6, 3, 5, 5, 2, 6, 5, 5, 3, 3, 4, 2, 2, 6, 1]
+            top_y = [0, 1, 0, 5, 2, 0, 2, 0, 0, 6, 0, 4, 6, 3, 5, 2, 6, 3, 3, 3, 5, 1, 1, 2, 4, 2, 1, 1, 3, 0, 5, 3]
+            mapper_x = top_x[:num_freq]
+            mapper_y = top_y[:num_freq]
+        elif "low" in method:
+            low_x = [0, 0, 1, 1, 0, 2, 2, 1, 2, 0, 3, 4, 0, 1, 3, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4]
+            low_y = [0, 1, 0, 1, 2, 0, 1, 2, 2, 3, 0, 0, 4, 3, 1, 5, 4, 3, 2, 1, 0, 6, 5, 4, 3, 2, 1, 0, 6, 5, 4, 3]
+            mapper_x = low_x[:num_freq]
+            mapper_y = low_y[:num_freq]
+        elif "bot" in method:
+            bot_x = [6, 1, 3, 3, 2, 4, 1, 2, 4, 4, 5, 1, 4, 6, 2, 5, 6, 1, 6, 2, 2, 4, 3, 3, 5, 5, 6, 2, 5, 5, 3, 6]
+            bot_y = [6, 4, 4, 6, 6, 3, 1, 4, 4, 5, 6, 5, 2, 2, 5, 1, 4, 3, 5, 0, 3, 1, 1, 2, 4, 2, 1, 1, 5, 3, 3, 3]
+            mapper_x = bot_x[:num_freq]
+            mapper_y = bot_y[:num_freq]
+        else:
+            raise NotImplementedError
+        return mapper_x, mapper_y
+
+
+class MultiSpectralDCTLayer(nn.Module):
+    """Generate dct filters for FCANet."""
+
+    def __init__(self, height, width, mapper_x, mapper_y, channel):
+        super(MultiSpectralDCTLayer, self).__init__()
+
+        assert len(mapper_x) == len(mapper_y)
+        assert channel % len(mapper_x) == 0
+
+        self.num_freq = len(mapper_x)
+
+        # fixed DCT init
+        self.register_buffer("weight", self.get_dct_filter(height, width, mapper_x, mapper_y, channel))
+
+        # fixed random init
+        # self.register_buffer('weight', torch.rand(channel, height, width))
+
+        # learnable DCT init
+        # self.register_parameter('weight', self.get_dct_filter(height, width, mapper_x, mapper_y, channel))
+
+        # learnable random init
+        # self.register_parameter('weight', torch.rand(channel, height, width))
+
+        # num_freq, h, w
+
+    def forward(self, x):
+        assert len(x.shape) == 4, "x must been 4 dimensions, but got " + str(len(x.shape))
+        # n, c, h, w = x.shape
+
+        x = x * self.weight
+
+        result = torch.sum(x, dim=[2, 3])
+        return result
+
+    def build_filter(self, pos, freq, POS):
+        result = math.cos(math.pi * freq * (pos + 0.5) / POS) / math.sqrt(POS)
+        if freq == 0:
+            return result
+        else:
+            return result * math.sqrt(2)
+
+    def get_dct_filter(self, tile_size_x, tile_size_y, mapper_x, mapper_y, channel):
+        dct_filter = torch.zeros(channel, tile_size_x, tile_size_y)
+
+        c_part = channel // len(mapper_x)
+
+        for i, (u_x, v_y) in enumerate(zip(mapper_x, mapper_y)):
+            for t_x in range(tile_size_x):
+                for t_y in range(tile_size_y):
+                    dct_filter[i * c_part: (i + 1) * c_part, t_x, t_y] = self.build_filter(
+                        t_x, u_x, tile_size_x
+                    ) * self.build_filter(t_y, v_y, tile_size_y)
+
+        return dct_filter
+
+
+class ECANet(nn.Module):
+    """Constructs Efficient Channel Attention Module.
+
+    Args:
+        kernel_size: Adaptive selection of kernel size
+
+    References:
+        Wang, Qilong and Wu, Banggu and Zhu, Pengfei and Li, Peihua and Zuo, Wangmeng and Hu, Qinghua. "ECA-Net:
+        Efficient Channel Attention for Deep Convolutional Neural Networks." In Proceedings of the IEEE/CVF Conference
+        on Computer Vision and Pattern Recognition (CVPR), pp. 11534-11542. 2020.
+        https://github.com/BangguWu/ECANet/blob/master/models/eca_module.py
+
+
+    """
+
+    def __init__(self, kernel_size=3):
+        super(ECANet, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=self.kernel_size, padding=(self.kernel_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)  # batch x channel x height x weight
+
+        # Two different branches of ECA module
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+
+        # Multi-scale information fusion
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
+
+
+class ECANetVideo(ECANet):
+    """This module extends ECANet for videos."""
+
+    def __init__(self, kernel_size=3):
+        super(ECANetVideo, self).__init__(kernel_size)
+        self.avg_pool = nn.AdaptiveAvgPool3d(1)
 
 
 class STAM(SELayer):
--- tests/embed/test_video_selayer.py
+++ tests/embed/test_video_selayer.py
@@ -1,37 +1,68 @@
+from math import log
+
 import torch
 
-from kale.embed.video_selayer import CBAM, CBAMVideo, SRM, SRMVideo, STAM
+from kale.embed.video_selayer import CBAM, CBAMVideo, FCANet, SRM, SRMVideo, STAM, ECANet, ECANetVideo
 
 # Dummy data: [batch_size, channel, time, height, width]
-INPUT_5D = torch.randn(2, 3, 16, 32, 32)
-INPUT_4D = torch.randn(2, 3, 32, 32)
+INPUT_5D = torch.randn(2, 64, 16, 32, 32)
+# Dummy data: [batch_size, channel, height, width]
+INPUT_4D = torch.randn(2, 64, 32, 32)
 
 
-def test_cbam_layer():
+def test_cbam():
     layer = CBAM(INPUT_4D.shape[1], 2)
     output = layer(INPUT_4D)
-    assert output.shape == (2, 3, 32, 32)
+    assert output.shape == (2, 64, 32, 32)
 
 
-def test_cbam_video_layer():
+def test_cbam_video():
     layer = CBAMVideo(INPUT_5D.shape[1], 2)
     output = layer(INPUT_5D)
-    assert output.shape == (2, 3, 16, 32, 32)
+    assert output.shape == (2, 64, 16, 32, 32)
 
 
-def test_stam_layer():
+def test_stam():
     layer = STAM(INPUT_5D.shape[1], 2)
     output = layer(INPUT_5D)
-    assert output.shape == (2, 3, 16, 32, 32)
+    assert output.shape == (2, 64, 16, 32, 32)
 
 
-def test_srm_layer():
+def test_srm():
     layer = SRM(INPUT_4D.shape[1])
     output = layer(INPUT_4D)
-    assert output.shape == (2, 3, 32, 32)
+    assert output.shape == (2, 64, 32, 32)
 
 
-def test_srm_video_layer():
+def test_srm_video():
     layer = SRMVideo(INPUT_5D.shape[1])
     output = layer(INPUT_5D)
-    assert output.shape == (2, 3, 16, 32, 32)
+    assert output.shape == (2, 64, 16, 32, 32)
+
+
+def test_fcanet():
+    layer = FCANet(INPUT_4D.shape[1], 16, 16)
+    output = layer(INPUT_4D)
+    assert output.shape == (2, 64, 32, 32)
+
+
+def test_ecanet():
+    C = INPUT_4D.shape[1]
+    b = 1
+    gamma = 2
+    t = int(abs((log(C, 2) + b) / gamma))
+    k = t if t % 2 else t + 1
+    layer = ECANet(k)
+    output = layer(INPUT_4D)
+    assert output.shape == (2, 64, 32, 32)
+
+
+def test_ecanet_video():
+    C = INPUT_5D.shape[1]
+    b = 1
+    gamma = 2
+    t = int(abs((log(C, 2) + b) / gamma))
+    k = t if t % 2 else t + 1
+    layer = ECANetVideo(k)
+    output = layer(INPUT_5D)
+    assert output.shape == (2, 64, 16, 32, 32)
