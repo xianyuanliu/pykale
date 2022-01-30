3881da7a1204b1648a7b8ea4a97822c4c3dc96f3
Xianyuan Liu <19544506+XianyuanLiu@users.noreply.github.com>
2022-01-30 16:54:30 +0800

    EPIC UDA challenge mode test

--- kale/pipeline/video_domain_adapter.py
+++ kale/pipeline/video_domain_adapter.py
@@ -25,7 +25,7 @@ from kale.pipeline.domain_adapter import (
     WDGRLTrainer,
 )
 
-# from kale.utils.logger import save_results_to_json
+from kale.utils.logger import save_results_to_json
 
 
 def create_mmd_based_video(
@@ -158,9 +158,9 @@ class BaseAdaptTrainerVideo(BaseAdaptTrainer):
         metrics_at_test = self.create_metrics_log("test")
 
         # Uncomment to save output to json file for EPIC UDA 2021 challenge.(3/3)
-        # save_results_to_json(
-        #     self.y_hat, self.y_t_hat, self.s_id, self.tu_id, self.y_hat_noun, self.y_t_hat_noun, self.verb, self.noun
-        # )
+        save_results_to_json(
+            self.y_hat, self.y_t_hat, self.s_id, self.tu_id, self.y_hat_noun, self.y_t_hat_noun, self.verb, self.noun
+        )
 
         log_dict = get_aggregated_metrics(metrics_at_test, outputs)
 
@@ -534,12 +534,12 @@ class DANNTrainerVideo(BaseAdaptTrainerVideo, DANNTrainer):
         self.audio_feat = self.feat["audio"]
 
         # Uncomment to store output for EPIC UDA 2021 challenge.(1/3)
-        # self.y_hat = []
-        # self.y_hat_noun = []
-        # self.y_t_hat = []
-        # self.y_t_hat_noun = []
-        # self.s_id = []
-        # self.tu_id = []
+        self.y_hat = []
+        self.y_hat_noun = []
+        self.y_t_hat = []
+        self.y_t_hat_noun = []
+        self.s_id = []
+        self.tu_id = []
 
     def forward(self, x):
         if self.feat is not None:
@@ -664,14 +664,14 @@ class DANNTrainerVideo(BaseAdaptTrainerVideo, DANNTrainer):
         adv_loss = loss_dmn_src + loss_dmn_tgt  # adv_loss = src + tgt
         log_metrics.update({f"{split_name}_source_domain_acc": dok_src, f"{split_name}_target_domain_acc": dok_tgt})
 
-        # # Uncomment to store output for EPIC UDA 2021 challenge.(2/3)
-        # if split_name == "test":
-        #     self.y_hat.extend(y_hat[0].tolist())
-        #     self.y_hat_noun.extend(y_hat[1].tolist())
-        #     self.y_t_hat.extend(y_t_hat[0].tolist())
-        #     self.y_t_hat_noun.extend(y_t_hat[1].tolist())
-        #     self.s_id.extend(s_id)
-        #     self.tu_id.extend(tu_id)
+        # Uncomment to store output for EPIC UDA 2021 challenge.(2/3)
+        if split_name == "test":
+            self.y_hat.extend(y_hat[0].tolist())
+            self.y_hat_noun.extend(y_hat[1].tolist())
+            self.y_t_hat.extend(y_t_hat[0].tolist())
+            self.y_t_hat_noun.extend(y_t_hat[1].tolist())
+            self.s_id.extend(s_id)
+            self.tu_id.extend(tu_id)
 
         return task_loss, adv_loss, log_metrics
 
