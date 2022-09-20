7cacefec28d8ca75095cff911b16ff4cf8ca7231
Xianyuan Liu <19544506+XianyuanLiu@users.noreply.github.com>
2022-09-20 11:41:12 +0800

    correct top5 accuracy

--- kale/pipeline/domain_adapter.py
+++ kale/pipeline/domain_adapter.py
@@ -67,10 +67,11 @@ def get_aggregated_metrics(metric_name_list, metric_outputs):
             metric_value = torch.stack([x[metric_name] for x in metric_outputs]).mean()
         else:
             # # get the correct top5 accuracy
-            if "top5" in metric_name and "action" not in metric_name:
-                metric_value = torch.mul(torch.cat([x[metric_name] for x in metric_outputs]).double().mean(), 5)
-            else:
-                metric_value = torch.cat([x[metric_name] for x in metric_outputs]).double().mean()
+            metric_value = torch.cat([x[metric_name] for x in metric_outputs]).double().mean()
+            # if "top5" in metric_name and "action" not in metric_name:
+            #     metric_value = torch.mul(torch.cat([x[metric_name] for x in metric_outputs]).double().mean(), 5)
+            # else:
+            #     metric_value = torch.cat([x[metric_name] for x in metric_outputs]).double().mean()
         metric_dict[metric_name] = metric_value.item()
     return metric_dict
 
